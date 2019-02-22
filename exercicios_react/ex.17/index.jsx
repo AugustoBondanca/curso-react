a4ac6f7ea792aecfcd6937b307abed2e06e02d3d
AugustoBondanca <augustobondancaa@gmail.com>
2019-02-22 11:04:11 -0300

    integrated redux-react

--- exercicios_react/ex.17/index.jsx
+++ exercicios_react/ex.17/index.jsx
@@ -3,10 +3,9 @@ import ReactDom from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import Field from './field'
-import fieldReducer from './fieldReducer'
 
 const reducers = combineReducers({
-    field: fieldReducer
+    field: () => ({ value: 'Opa '})
 })
 
 ReactDom.render(
